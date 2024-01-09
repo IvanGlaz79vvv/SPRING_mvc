b3e9b3f2c9bb7783b1c55a19a21f6fe76de11f2a
IvanTest-31 <ivangl79@gmail.com>
2024-01-10 02:45:35 +0300

    ИЗМЕНИЛИ класс контроллера. После получения результатов заполнения формы, данные будут сохраняться в базе данных.

--- src/main/java/org/example/spring_mvc/Student — old.java
+++ src/main/java/org/example/spring_mvc/Student — old.java
@@ -1,79 +1,81 @@
-//package org.example.spring_mvc;
-//
-//public class Student {
-//
-//    private long id;
-//    private String lastName;
-//    private String firstName;
-//    private String patronymic;
-//    private String email;
-//    private String phone;
-//    private String address;
-//
-//    public long getId() {
-//        return id;
-//    }
-//
-//    public void setId(long id) {
-//        this.id = id;
-//    }
-//
-//    public String getLastName() {
-//        return lastName;
-//    }
-//
-//    public void setLastName(String lastName) {
-//        this.lastName = lastName;
-//    }
-//
-//    public String getFirstName() {
-//        return firstName;
-//    }
-//
-//    public void setFirstName(String firstName) {
-//        this.firstName = firstName;
-//    }
-//
-//    public String getPatronymic() {
-//        return patronymic;
-//    }
-//
-//    public void setPatronymic(String patronymic) {
-//        this.patronymic = patronymic;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public String getPhone() {
-//        return phone;
-//    }
-//
-//    public void setPhone(String phone) {
-//        this.phone = phone;
-//    }
-//
-//    public String getAddress() {
-//        return address;
-//    }
-//
-//    public void setAddress(String address) {
-//        this.address = address;
-//    }
-//
-//    public Student() {}
-//
-//    public Student(String lastName, String firstName, String patronymic, String email, String phone, String address) {
-//        this.lastName = lastName;
-//        this.firstName = firstName;
-//        this.patronymic = patronymic;
-//        this.email = email;
-//        this.phone = phone;
-//        this.address = address;
-//    }
-//}
+/*
+package org.example.spring_mvc;
+
+public class Student {
+
+    private long id;
+    private String lastName;
+    private String firstName;
+    private String patronymic;
+    private String email;
+    private String phone;
+    private String address;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Student() {}
+
+    public Student(String lastName, String firstName, String patronymic, String email, String phone, String address) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.patronymic = patronymic;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
+}
+*/
--- src/main/java/org/example/spring_mvc/StudentController.java
+++ src/main/java/org/example/spring_mvc/StudentController.java
@@ -1,13 +1,20 @@
 package org.example.spring_mvc;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-
+//Изменим класс контроллера. После получения результатов заполнения формы, данные будут сохраняться в базе данных.
 @Controller
 public class StudentController {
+    private StudentService service;
+
+    @Autowired
+    public void setService(StudentService service) {
+        this.service = service;
+    }
 
     @GetMapping("/")
     public String index(Model model) {
@@ -21,6 +28,7 @@ public class StudentController {
     }
     @PostMapping("/add_student")
     public String greetingSubmit(@ModelAttribute Student student) {
+        service.saveStudent(student);
         return "redirect:/";
     }
 }
